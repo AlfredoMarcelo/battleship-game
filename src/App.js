b7bbcca4f6aa348acfc73769b68433e2f5b89117
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-07-25 11:16:51 -0400

    se agrega componente nombre y su hoja de estilo para centrar bajo los tableros

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import "./hojas-de-estilos/Nombre.css"
 import TablaJugador from "./componentes/TablaJugador";
 import TablaCpu from "./componentes/TablaCpu"
 import buqueLogo from "./imagenes/buque.png";
+import Nombre from "./componentes/Nombre";
 
 function App() {
-
   return (
     <div className="contenedor-principal">
       <div className="contenedor-titulo">
@@ -17,6 +18,14 @@ function App() {
       <div className="contenedor-tablas">
           <TablaCpu />
           <TablaJugador/>
+      </div>
+      <div className="contenedor-principal-nombres">
+        <div className="contenedor-nombre">
+          <Nombre nombre="Cpu"/>
+        </div>
+        <div className="contenedor-nombre">
+          <Nombre nombre="Saul"/>
+        </div>
       </div>
     </div>
   );
--- src/componentes/TablaJugador.js
+++ src/componentes/TablaJugador.js
@@ -7,16 +7,19 @@ function TablaJugador(){
   const [tableroJugador, setTableroJugador] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 1, 1, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ]);
   
+   /*obtenerDatoJugador() recibe la posicion del recuadro seleccionado y la setea con 2(tiro fallado) si es 0(mar) 
+  y con 3(tiro que hizo daño) si es 1(buque) */
+
   const obtenerDatoJugador = (index, index2) =>{
     let copiaTablaJugador = [...tableroJugador];
     if(copiaTablaJugador[index][index2] === 0){
@@ -25,6 +28,20 @@ function TablaJugador(){
       copiaTablaJugador[index][index2] = 3;
     }
     setTableroJugador(copiaTablaJugador);
+    revisarArray();
+  }
+
+  /* revisarArray() es ejecutada en cada clic para verificar si quedan 1(barcos) en el arreglo
+  , si no encuentra arrojara la ventana con el ganador*/
+
+  function revisarArray(){
+    let copiaTablaJugador = tableroJugador;
+    let contadorDeBarco = 0;
+    copiaTablaJugador.forEach((primerElemento)=>primerElemento.forEach((segundoElemento)=>segundoElemento === 1? contadorDeBarco += 1:''))    
+    if(contadorDeBarco === 0){
+      alert("El ganor fue el Cpu");
+      setTableroJugador([[]]);
+    }
   }
 
   return(
